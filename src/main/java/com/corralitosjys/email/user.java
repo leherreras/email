96d178388ca19a7d7f2daaf852732c3c6244ddad
leherreras <leherreras@gmail.com>
2016-07-13 16:18:44 -0500

    Envio de emails basico

---
+++ src/main/java/com/corralitosjys/email/user.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corralitosjys.email;
+
+import java.io.Serializable;
+import java.util.Properties;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+/**
+ *
+ * @author Luis Herrera
+ */
+@Named(value = "user")
+@ViewScoped
+public class user implements Serializable {
+
+    private String usuarioCorreo = "leherreras@gmail.com";
+    private String password = "jcwysprerpyeqxwg";
+    private String asunto;
+    private String cuerpo;
+    private Properties p;
+    private String correo = "leherreras@gmail.com";
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuarioCorreo() {
+        return usuarioCorreo;
+    }
+
+    public void setUsuarioCorreo(String usuarioCorreo) {
+        this.usuarioCorreo = usuarioCorreo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    @PostConstruct
+    public void init() {
+        p = new Properties();
+
+    }
+
+    public void enviarCorreo() {
+        try {
+
+            p.put("mail.smtp.host", "smtp.gmail.com");
+            p.setProperty("mail.smtp.starttls.enable", "true");
+            p.setProperty("mail.smtp.port", "587");
+            p.setProperty("mail.smpt.user", this.getUsuarioCorreo());
+            p.setProperty("mail.smtp.auth", "true");
+
+            Session s = Session.getDefaultInstance(p, null);
+            BodyPart texto = new MimeBodyPart();
+            texto.setText(this.getCuerpo());
+
+            MimeMultipart m = new MimeMultipart();
+            m.addBodyPart(texto);
+
+            MimeMessage mensaje = new MimeMessage(s);
+            mensaje.setFrom(new InternetAddress(this.getUsuarioCorreo()));
+            mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(this.getCorreo()));
+            mensaje.setSubject(this.getAsunto());
+            mensaje.setContent(m);
+
+            Transport t = s.getTransport("smtp");
+            t.connect(this.getUsuarioCorreo(), this.getPassword());
+            t.sendMessage(mensaje, mensaje.getAllRecipients());
+            t.close();
+
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Se envio el correo"));
+        } catch (Exception e) {
+
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Aviso", "No se envio el correo"));
+        }
+
+    }
+
+}
